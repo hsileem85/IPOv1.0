f27168c8dc57bcc8bfc932ba4e7ee3b2a84b65bc
hsileem85 <hsileem85@gmail.com>
2026-05-11 02:27:17 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/IPOv1.0/",
   plugins: [react()],
-});
+})
