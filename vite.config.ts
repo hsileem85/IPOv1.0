1fe4d75d39b36633c4e0768208150b4b7e25ed52
hsileem85 <hsileem85@gmail.com>
2026-05-11 03:00:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/IPOv1.0/",   <--- هذا هو السطر الناقص!
+  base: "/IPOv1.0/",   
   plugins: [react()],
 })
